ff5c02a237d5e065b6a4c7df25d18ce0b80b0ea7
Etheiry <lateluffy@gmail.com>
2024-10-09 13:57:03 -0400

    beta

--- ticTacToe.cpp
+++ ticTacToe.cpp
@@ -1,108 +1,152 @@
 #include <iostream>
-using std::cout;
+using std::cout, std::cin;
 
 class TicTac {
-    int board[3][3] = {
-        {0,0,0},
-        {0,0,0},
-        {0,0,0},
+    char board[3][3] = {
+        {'.','.','.'},
+        {'.','.','.'},
+        {'.','.','.'},
     }; 
-    // int player1, player2;
-    int winner;
-    int round = 9;
 
-    public: 
-        bool winCheck() {
-        
-            for (int row = 0; row < 3; row++) {
-                for (int col = 0; col < 3; col++) {
+    
+    bool winCheck(char player) {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
 
-                    if (board[row][col] != 0) {
-                        // * Row Check
-                        if ((board[row][0] && board[row][1] && board[row][2]) == 2 || 1) {
-                            winner = board[row][0];
-                            return true;
-                        }
-                        // * Col Check
-                        if ((board[0][col] && board[1][col] && board[2][col]) == 2 || 1) {
-                            winner = board[0][col];
-                            return true;
-                        }
+                if (board[row][col] != 0) {
+                    // * Row Check
+                    if (board[row][0] == player && board[row][1] == player && board[row][2] == player) { 
+                        return true;
+                    } 
+                    // * Col Check
+                    if (board[0][col] == player && board[1][col] == player && board[2][col] == player) {
+                        return true;
+                    }
 
-                        //* dia Check
-                        if ((board[0][0] && board[1][1] && board[2][2]) == 2 || 1) {
-                            winner = board[0][0];
-                            return true;
-                        }
-                        if ((board[0][2] && board[1][1] && board[2][0]) == 2 || 1) {
-                            winner = board[0][2];
-                            return true;
-                        }
+                    //* diagonal Check
+                    if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+                        return true;
+                    }
+                    if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+                        return true;
                     }
                 }
-            
-            return false;
-            
-        }
-
+            }
         
-
-
+        return false;
+        
+        }
     }
 
-        int outputWinner() {
-            //* outputs winner
-            if (winner == 1) {
-                cout << "Player 1 wins!" << "\n";
-            }
-            else if (winner == 2)
-            {
-                cout << "Player 2 wins" << "\n";
-            }
-            else {
-                cout << "Tied!" << "\n";
-            }
+    public: bool outputWinner() {
+        //* outputs winner
+        if (winCheck('X')) {
+            cout << "Player 1 wins!" << "\n";
+            return true;
+        }
+        else if (winCheck('O'))
+        {
+            cout << "Player 2 wins" << "\n";
+            return true;
+        }
+    }
+
+    // player controller  
+    
+    public: void player(int playerPosition) {
+        char playerSymbol;
+        int row, col;
+
+        if (playerPosition == 1) {
+            playerSymbol = 'X';
+        } else if (playerPosition == 2) {
+            playerSymbol = 'O';
         }
 
-    //* Refactor player controller into a single method    
-    public:
-        void player1() {
-            int row = 1, col=2;
-            cout << "Player 1 choose row: ";
-            // std::cin >> row;
+        // Get player position input
+        cout << "Player " << playerPosition << "(" << playerSymbol << ")" << " choose row: ";   
+        cin >> row;
+        
+        cout << "Player " << playerPosition << "(" << playerSymbol << ")" << " choose col: ";
+        cin >> col;
+        
+        
+        
 
-            cout << "Player 1 choose col: ";
-            // std::cin >> col;
-
-            row--;
-            col--;
-            board[row][col] = 1;
-
+        row--;
+        col--;
+        if (board[row][col] == '.' && playerSymbol == 'X') {
+            board[row][col] = 'X';
+        } 
+        else if (board[row][col] == '.' && playerSymbol == 'O'){
+            board[row][col] = 'O';  
+        } 
+        else {
+            cout << "Spot already taken!\n";
+            player(playerPosition);
         }
-    public:
-        void player2() {
-            int row, col;
-            cout << "Player 1 choose row: ";
-            std::cin >> row;
+    }
+    //     void player1() {
+    //         // int row = 1, col=2;
+    //         int row, col;
+    //         cout << "Player 1 choose row: ";
+    //         std::cin >> row;
 
-            cout << "Player 1 choose col: ";
-            std::cin >> col;
+    //         cout << "Player 1 choose col: ";
+    //         std::cin >> col;
 
-            row--;
-            col--;
-            board[row][col] = 2;
+    //         row--;
+    //         col--;
+    //         if (board[row][col] == '.') {
+    //             board[row][col] = 'X';
+    //         } else {
+    //             player1();
+    //         }
 
+    //     }
+    // public:
+    //     void player2() {
+    //         int row, col;
+    //         cout << "Player 2 choose row: ";
+    //         std::cin >> row;
+
+    //         cout << "Player 2 choose col: ";
+    //         std::cin >> col;
+
+    //         row--;
+    //         col--;
+    //         if (board[row][col] == '.') {
+    //             board[row][col] = 'O';
+    //         } else {
+    //             player2();
+    //         }
+
+    //     }
+
+    
+    //* utils (optional) 
+    public: void DisplayBoard() {
+        for (auto &row : board) {
+            for (char val : row) {
+                cout << val;
+            }
+            cout << "\n";
         }
+    }
 
-    public:
-        void DisplayBoard() {
-            for (auto &row : board) {
-                for (int val : row) {
-                    cout << val;
-                }
-                cout << "\n";
+    //! NOT IN USE
+    bool validatePosition(int pos) {
+        try {
+            if (1 <= pos >= 3) {
+                throw -1;
             }
         }
+        catch (...) {
+            cout << "Invalid position!";
+            return false;
+        }
+        return true;
+    }
 
 };
 
@@ -113,32 +157,45 @@ class TicTac {
 
 
 int main() {
-    int rounds = 1;
     TicTac game;
+    int player1 = 1, player2 = 2;
 
+
+    // keeps track of rounds 
+    int rounds = 1;
+    int maxRounds = 9;
+
+    // guide to rows and columns
+    const char* boardGuide ="        Col\n"
+                            "       1 2 3\n"
+                            "   row 2    \n"
+                            "       3    \n";
+    cout << boardGuide;
+    
     
     while (true) {
         //* If winner decided: outputs winner and stops game
-        if (game.winCheck()) {
-            game.outputWinner();
-            game.DisplayBoard();
+        if (game.outputWinner()) {
             break;
         }
-
-        if (rounds % 2 != 0 && rounds <= 10) {
-            game.player1();
+        
+        //* Decides whos turn it is depending on the round 
+        if (rounds % 2 != 0 && rounds <= maxRounds) {
+            game.player(player1);
+            game.DisplayBoard();
+            rounds++;
+        } else if (rounds % 2 == 0 && rounds <= maxRounds) {
+            game.player(player2);
+            game.DisplayBoard();
             rounds++;
         } else {
-            game.player2();
-            rounds++;
+            cout << "Game Over!" << "\n";
+            cout << "Tied Game!";
+            break;
         }
-        game.DisplayBoard();
+        
 
     }
-    
-    cout << "Game over!";
-    
-    
     
     return 0;
 }
